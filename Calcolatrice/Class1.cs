471143702be76689a6afa3703baad280dc06b568
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-10-31 13:45:59 +0100

    funzione clone

--- Calcolatrice/Class1.cs
+++ Calcolatrice/Class1.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Calcolatrice
 {
-    internal class Class1
+    internal class Calc : ICloneable
     {
+        private double _num1;
+        private double _num2;
+
+
+        public double Num1 { set { _num1 = value; } get { return _num1; } }
+        public double Num2 { set { _num2 = value; } get { return _num2; } }
+
+
+        public Calc()
+        {
+            Num1 = 0;
+            Num2 = 0;
+        }
+
+        public Calc(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public Calc(Calc calc)
+        {
+            Num1 = calc.Num1;
+            Num2 = calc.Num2;
+        }
+
+        public Object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+
+        public double Somma()
+        {
+            return Num1 + Num2;
+        }
+
+        public double Sottrazione()
+        {
+            return Num1 + Num2;
+        }
     }
 }
